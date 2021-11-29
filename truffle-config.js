27a3f6ef5253fa153eff13734b2a197d100be38a
Sebastien V <bleuplatine@gmail.com>
2021-11-29 10:15:05 +0100

    Ajout network ropsten, prepa deploy

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
@@ -9,6 +9,14 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
   },
   mocha: {
